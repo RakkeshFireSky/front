6a32135f2797c3e2ab476cfdbd21e74111101b0b
RakkeshFireSky <rakkeshkumar@firesky.cc>
2026-01-07 13:29:40 +0530

    Check Auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,35 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-// @ts-ignore
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+// // @ts-ignore
 
-export function middleware(req: NextRequest) {
-  const token = req.cookies.get("accessToken")?.value;
-  console.log("Middleware Token:", token);
-  const isAuthPage = req.nextUrl.pathname.startsWith("/sign-in");
-  const isProtectedRoute = req.nextUrl.pathname.startsWith("/text-summarize");
+// export function middleware(req: NextRequest) {
+//   const token = req.cookies.get("accessToken")?.value;
+//   console.log("Middleware Token:", token);
+//   const isAuthPage = req.nextUrl.pathname.startsWith("/sign-in");
+//   const isProtectedRoute = req.nextUrl.pathname.startsWith("/text-summarize");
 
-  // 🚫 Not logged in → block protected routes
-  if (!token && isProtectedRoute) {
-    return NextResponse.redirect(
-      new URL("/sign-in", req.url)
-    );
-  }
+//   // 🚫 Not logged in → block protected routes
+//   if (!token && isProtectedRoute) {
+//     return NextResponse.redirect(
+//       new URL("/sign-in", req.url)
+//     );
+//   }
 
-  // 🔁 Logged in → prevent access to auth pages
-  if (token && isAuthPage) {
-    return NextResponse.redirect(
-      new URL("/text-summarize", req.url)
-    );
-  }
+//   // 🔁 Logged in → prevent access to auth pages
+//   if (token && isAuthPage) {
+//     return NextResponse.redirect(
+//       new URL("/text-summarize", req.url)
+//     );
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: [
-    "/text-summarize",
-    "/text-summarize/:path*",
-    "/sign-in",
-  ],
-};
+// export const config = {
+//   matcher: [
+//     "/text-summarize",
+//     "/text-summarize/:path*",
+//     "/sign-in",
+//   ],
+// };
 
